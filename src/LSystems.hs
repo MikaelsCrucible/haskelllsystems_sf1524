2d0e967fa7a8233a96c5d0511080ae42e0444a05
Sihan Fu <sf1524@ic.ac.uk>
2024-10-25 18:38:22 +0100

    test

--- src/LSystems.hs
+++ src/LSystems.hs
@@ -1,9 +1,10 @@
 module LSystems ( LSystem(LSystem), ColouredLine, Command(..)
                 , angle, axiom, rules, lookupChar
-                , expandOne, expand, move, parse, trace1, trace2
+                , expandOne, expand, move, parse,unbracket, trace1, trace2
                 , expandLSystem, commandMap ) where
 
 import IC.Colour
+import GHC.RTS.Flags (MiscFlags(installSignalHandlers))
 
 type Rules a = [(Char, [a])]
 data LSystem = LSystem Float [Char] (Rules Char)
@@ -12,38 +13,44 @@ type TurtleState = (Vertex, Float)
 data Command = F | L | R | B [Command] deriving Show
 type ColouredLine = (Vertex, Vertex, Colour)
 
+--rulesPeanoGosper :: Rules Char
+--rulesPeanoGosper = [('M', "M+N++N-M--MM-N+"), ('N', "M-N++N+M--M-N")] --for debug
+
 ----------------------------------------------------------
 -- Functions for working with systems.
 
 -- Returns the rotation angle for the given system.
 angle :: LSystem -> Float
-angle = undefined
+angle (LSystem ang _ _) = ang
 
 -- Returns the axiom string for the given system.
 axiom :: LSystem -> [Char]
-axiom = undefined
+axiom (LSystem _ axi _) = axi
 
 -- Returns the set of rules for the given system.
 rules :: LSystem -> Rules Char
-rules = undefined
+rules (LSystem _ _ rul) = rul
 
 --
 -- Pre: the character has a binding in the Rules list
 --
 lookupChar :: Rules a -> Char -> [a]
-lookupChar = undefined
+lookupChar rule c = case lookup c rule of
+                      Just rep -> rep
+                      Nothing -> []
 
 --
 -- Expand command string s once using rule table r
 --
 expandOne :: Rules Char -> [Char] -> [Char]
-expandOne = undefined
+expandOne rules = concatMap (lookupChar rules)
 
 --
 -- Expand command string s n times using rule table r
 --
 expand :: [Char] -> Int -> Rules Char -> [Char]
-expand = undefined
+expand str 0 _ = str
+expand str n rule = expand (expandOne rule str) (n - 1) rule
 
 -- Move a turtle.
 --
@@ -51,20 +58,66 @@ expand = undefined
 -- L rotates left according to the given angle.
 -- R rotates right according to the given angle.
 move :: Command -> Float -> TurtleState -> TurtleState
-move = undefined
+move L ang ((x, y), facing) = ((x, y), facing + ang)
+move R ang ((x, y), facing) = ((x, y), facing - ang)
+move F _ ((x, y), facing) = ((x + 1 * cos rad, y + 1 * sin rad), facing) where rad = degreesToRadians facing
 
 degreesToRadians :: Float -> Float
 degreesToRadians x = (x / 180) * pi
 
 parse :: Rules Command -> [Char] -> [Command]
-parse = undefined
+parse _ [] = []
+parse rules ('[' : str) = let cmd = parse rules inside
+                              rest = parse rules outside
+                          in B cmd : rest
+                            where (inside, outside) = unbracket 1 str
+parse rules (c : cs) = lookupChar rules c ++ parse rules cs
+
+unbracket :: Integer -> [Char] -> ([Char], [Char]) -- return the stuff inside [] and what is left at the right side of ]
+unbracket _ [] = ([], [])
+unbracket 1 (']' : str) = ([], str)
+unbracket n (']' : str) = 
+  let (inside, outside) = unbracket (n - 1) str
+  in (']' : inside, outside) 
+unbracket n ('[' : str) = 
+  let (inside, outside) = unbracket (n + 1) str
+  in ('[' : inside, outside)
+unbracket n (c : str) = 
+  let (inside, outside) = unbracket n str
+  in (c : inside, outside)
 
 trace1 :: [Command] -> Float -> Colour -> [ColouredLine]
-trace1 = undefined
+trace1 cmds = tracehelper1 cmds ((0, 0), 90) []
+
+tracehelper1 :: [Command] -> TurtleState -> [ColouredLine] -> Float -> Colour -> [ColouredLine]
+tracehelper1 [] _ lines _ _ = lines
+tracehelper1 (F : cs) ((x, y), facing) lines ang colour = let ((newx, newy), facing) = move F ang ((x, y), facing)
+                                                              line = ((x, y), (newx, newy), colour)
+                                                          in tracehelper1 cs ((newx, newy), facing) (line : lines) ang colour 
+tracehelper1 (L : cs) ((x, y), facing) lines ang colour = let (_, newfacing) = move L ang ((x, y), facing)
+                                                          in tracehelper1 cs ((x, y), newfacing) lines ang colour
+tracehelper1 (R : cs) ((x, y), facing) lines ang colour = let (_, newfacing) = move R ang ((x, y), facing)
+                                                          in tracehelper1 cs ((x, y), newfacing) lines ang colour
+tracehelper1 (B cmds : cs) state lines ang colour = let insidelines = tracehelper1 cmds state [] ang colour
+                                                         in tracehelper1 cs state (lines ++ insidelines) ang colour
+
 
 -- This version uses an explicit stack of residual commands and turtle states
 trace2 :: [Command] -> Float -> Colour -> [ColouredLine]
-trace2 = undefined
+trace2 cmds = tracehelper2 cmds [] ((0, 0), 90) []
+
+tracehelper2 :: [Command] -> [(TurtleState, [Command])] -> TurtleState -> [ColouredLine] -> Float -> Colour -> [ColouredLine]
+tracehelper2 [] [] _ lines _ _ = lines
+tracehelper2 [] ((state, cmds) : stack) ((x, y), facing) lines ang colour = tracehelper2 cmds stack state lines ang colour
+tracehelper2 (F : cs) stack ((x, y), facing) lines ang colour = let ((newx, newy), facing) = move F ang ((x, y), facing)
+                                                                    line = ((x, y), (newx, newy), colour)
+                                                                in tracehelper2 cs stack ((newx, newy), facing) (line : lines) ang colour
+tracehelper2 (L : cs) stack ((x, y), facing) lines ang colour = let (_, newfacing) = move L ang ((x, y), facing)
+                                                                in tracehelper2 cs stack ((x, y), newfacing) lines ang colour
+tracehelper2 (R : cs) stack ((x, y), facing) lines ang colour = let (_, newfacing) = move R ang ((x, y), facing)
+                                                                in tracehelper2 cs stack ((x, y), newfacing) lines ang colour
+tracehelper2 (B cmds : cs) stack state lines ang colour = tracehelper2 cs ((state, cmds) : stack) state lines ang colour
+
 
 -- Provided Functions
 ------------------------------------------------------------------------------
